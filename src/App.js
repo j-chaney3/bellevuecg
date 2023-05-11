4234dde2c5af3359e9217e3cf2544bc04ba3a2d0
Jame Chaney <macneilslt@gmail.com>
2023-05-11 04:06:56 -0400

    Added routes for navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { Routes, Route, Router } from 'react-router-dom';
 import React from 'react';
 import './App.css';
+import Header from '../src/components/Header'
+import Footer from '../src/components/Footer'
+import HomePage from './pages/HomePage';
+import ReservePage from './pages/ReservePage';
+import MembersPage from './pages/MembersPage';
+import EventsPage from './pages/EventsPage';
 
 function App() {
-  return (
-    <>
-      <div>
-        Bellevue Community Garden.
-      </div>
-    </>
-  );
+
+	return (
+        
+		<div className="App">
+			<Header />
+			<Routes>
+				<Route path='events' element={<EventsPage />}/>
+				<Route path='reserve' element={<ReservePage/>} />
+				<Route path='members' element={<MembersPage/>} /> 
+				<Route path='/' element={<HomePage/>} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
