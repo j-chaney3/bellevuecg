c396aee63ce9448b2812fdb2cd64f01bcad9eaca
Jame Chaney <macneilslt@gmail.com>
2023-05-11 04:05:02 -0400

    slice selecting garden beds object

---
+++ src/features/slices/gardenbedsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { GARDENBEDS } from "../../app/shared/GARDENBEDS";
+
+const initialState = {
+    gardenbedsArray: GARDENBEDS
+};
+
+const gardenbedSlice = createSlice({
+    name: 'gardenbeds',
+    initialState
+})
+
+export const gardenbedsReducer = gardenbedSlice.reducer;
+
+export const sellectAllGardenbeds = (state) => {
+    return state.gardenbeds.gardenbedsArray;
+}
+
