b9f837f65be9ebdec3741a2e43882531475cc836
Jame Chaney <macneilslt@gmail.com>
2023-05-15 08:08:26 -0400

    updated object info

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { gardenbedsReducer } from '../features/slices/gardenbedsSlice';
 
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    
+    gardenbeds: gardenbedsReducer,
+  
   },
 });
